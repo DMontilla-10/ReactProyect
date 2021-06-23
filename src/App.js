b8b634c37f83e318a90861f82745012936aa3400
Daniel Montilla <montilla.daniel@reanswer.com.ar>
2021-06-22 22:01:54 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Padre from './components/Padre';
+import Contador from './components/Ejercicio/Contador/Contador';
+
+function App() {
+  return (
+    <>
+      <Padre />
+      <Contador />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Ejercicio/Contador/Contador.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import Botón from '../Botón';
+import Typography from '@material-ui/core/Typography';
+import { useStyles } from './style';
+
+export default function Contador() {
+    const classes = useStyles();
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className={classes.root}>
+            <Typography variant="h6" gutterBottom>
+                Clickeaste {count} veces
+            </Typography>
+            <Botón setCount={setCount} count={count} />
+        </div>
+    )
+}
+
